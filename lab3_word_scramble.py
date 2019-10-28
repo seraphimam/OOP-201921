57cb58233bf5aae7985f658fb805eebf6cc47b52
seraphimam <karlpoon2000@gmail.com>
2019-10-28 22:34:20 +0000

    lab 3

--- lab3_word_scramble.py
+++ lab3_word_scramble.py
@@ -5,6 +5,7 @@
 # purpose: Lab 3
 
 import string
+import random
 
 class WordScramble:
     def __init__(self):
@@ -12,10 +13,22 @@ class WordScramble:
 
     def scramble(self):
         # print what was input
-        print("The user input was: ", self.user_input)
 
-        # first scramble is just one word
-        # reverse two indices
+        print("The user input was: ", self.user_input)
+        user = list(self.user_input.split())
+        #
+        # # first scramble is just one word
+        #
+        random.shuffle(user)
+        print(user)
+        #
+        # # reverse two indices
+        #
+        temp = user[0]
+        user[0] = user[-1]
+        user[-1] = temp
+        print(user)
+
         # particularly good to use is to switch the first two
         # and the last two
         # this only makes sense if you have a world that is longer than 3
@@ -24,6 +37,21 @@ class WordScramble:
         # now try to scramble one sentence
         # do just words first, then you can move on to work on
         # punctuation
+        new = list(self.user_input.split())
+
+        for i, word in enumerate(new):
+            if len(word) > 3:
+                ran = list(word)
+                random.shuffle(ran)
+                print(ran)
+
+                s = ""
+                for x in ran:
+                    s += x
+
+            new[i] = s
+
+        print(new)
 
 word_scrambler = WordScramble()
 word_scrambler.scramble()
