f2833743963285f4f787f8cba6a4c5d9671da566
seraphimam <karlpoon2000@gmail.com>
2019-10-28 21:55:31 +0000

    lab2

--- lab2_types_and_strings.py
+++ lab2_types_and_strings.py
@@ -50,31 +50,54 @@ class Types_and_Strings:
         print(message[::2])
 
     def play_with_lists(self):
+        my_list1 = ['a', 'b', 'c']
+        my_list2 = [1, 2, 3]
+        my_list1.extend(my_list2)
+        print(my_list1)
+
         message = input("Please enter a whole sentence: ")
         print("Originally entered: "+ message)
 
         # hand the input string to a list and print it out
+        user = []
+        user += message.split()
+        print(user)
+        #for i in message:
+         #   user.append(i)
 
+        #print(*user)
 
         # append a new element to the list and print
-
+        user.append(" since")
+        print(*user, sep='')
 
         # remove from the list in 3 ways
+        del user[-1]
+        print(user)
 
+        user.pop(-1)
+        print(user)
+
+        user.remove('I')
+        print(user)
 
         # check if the word cake is in your input list
-
+        if user.count('cake') > 0:
+            print(True)
+        else:
+            print(False)
 
         # reverse the items in the list and print
-
+        user.reverse()
+        print(user)
 
         # reverse the list with the slicing trick
-
+        print(user[::-1])
 
         # print the list 3 times by using multiplication
-
+        print(user*3)
 
 
 tas = Types_and_Strings()
-tas.play_with_strings()
-#tas.play_with_lists()
+#tas.play_with_strings()
+tas.play_with_lists()
